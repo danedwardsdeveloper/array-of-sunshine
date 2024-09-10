a22f70401b381818afcf1da9e37824bd60ab1634
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-10 18:15:27 +0100

    Make date column fixed width

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -15,12 +15,11 @@ export default async function ArticlesList() {
 			{articles.map((article) => (
 				<Link
 					key={article.slug}
-					className="flex flex-col space-y-1 mb-4"
 					href={`/articles/${article.slug}`}
+					className="flex flex-col space-y-1 mb-4"
 				>
 					<div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-						{/* 150px might seem too wide but it accommodates wide dates like '23 September 2024' and allows the article titles to line up nicely*/}
-						<p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
+						<p className="text-neutral-600 dark:text-neutral-400 tabular-nums md:w-40 md:flex-shrink-0 md:mr-2">
 							{formatDate(article.date)}
 						</p>
 						<p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
