feef34f7bbf35f8c36316909695a186283d892eb
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-13 10:45:51 +0100

    Update page title

--- src/app/articles/getting-started-with-typescript-a-super-fast-guide/data.ts
+++ src/app/articles/getting-started-with-typescript-a-super-fast-guide/data.ts
@@ -6,7 +6,7 @@ import typeScriptImage from '../../../../public/images/social-png/typescript.png
 export const article: IArticle = {
 	writer: `Dan Edwards`,
 	date: `2024-09-12`,
-	title: `Getting started with TypeScript - a super fast guide`,
+	title: `Getting started with TypeScript: a super fast guide`,
 	description: `A concise guide on setting up a TypeScript project with minimal configuration, including project structure, dependency installation, and basic TypeScript usage.`,
 	keywords:
 		'TypeScript setup, pnpm, tsx, nodemon, tsconfig, Project initialization, Dependency management, TypeScript configuration, Development workflow',
