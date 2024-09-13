9f9e72eb701cc2bd3b526279f92ee35a4abd52ec
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-13 09:00:09 +0100

    Create article template

---
+++ article-template/data.ts
@@ -0,0 +1,19 @@
+import { Metadata } from 'next';
+
+import { type IArticle } from '@/library/articles';
+import exampleFeaturedImage from '../../../../public/images/social-png/mailchimp-email-api-typescript.png';
+
+export const article: IArticle = {
+	writer: `Dan Edwards`,
+	date: `2024-09-11`,
+	title: `Demonstration article with lots of images`,
+	description: `This is just a demonstration article with lots of images`,
+	keywords: `demo article`,
+	tags: 'history',
+	featuredImage: exampleFeaturedImage,
+};
+
+export const metadata: Metadata = {
+	title: article.title,
+	description: article.description,
+};
