e4115072b5f2ef0f6dca066831fe687a0f240a4b
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-13 10:45:41 +0100

    Update page title

--- src/app/articles/complex-react-root/data.ts
+++ src/app/articles/complex-react-root/data.ts
@@ -5,7 +5,7 @@ import reactImage from '../../../../public/images/social-png/complex-react-root-
 export const article: IArticle = {
 	writer: `Dan Edwards`,
 	date: '2024-06-24',
-	title: `Creating a Complex React Root | Order and Dependencies`,
+	title: `Creating a complex React root: order and dependencies`,
 	description: `Learn how to set up a complex React root with multiple providers, ensuring robust and maintainable applications. Understand the order and dependencies for a comprehensive React setup.`,
 	keywords: `complex react root, react setup, multiple providers, react dependencies, react order, redux, apollo provider, intlprovider, themeprovider, react-helmet, react-router, persistgate, react front-end, javascript, web development`,
 	tags: 'React, React Router, React Helmet',
