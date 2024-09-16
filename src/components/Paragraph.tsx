d7e8c003ca0e7b133edcdeeccf92aff7b5d1980c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-09-16 08:34:09 +0100

    Add background colour option

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,9 +1,30 @@
+import clsx from 'clsx';
+
+interface ParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {
+	background?: 'green' | 'red';
+}
+
+const baseStyles = 'my-4 text-neutral-800 dark:text-neutral-200';
+const baseBackgroundStyles = 'p-4 rounded-lg';
+
 export default function Paragraph({
+	background,
 	children,
+	className,
 	...props
-}: React.HTMLAttributes<HTMLParagraphElement>) {
+}: ParagraphProps) {
 	return (
-		<p className="my-4 text-neutral-800 dark:text-neutral-200" {...props}>
+		<p
+			className={clsx(
+				baseStyles,
+				{
+					[`${baseBackgroundStyles} bg-green-200`]: background === 'green',
+					[`${baseBackgroundStyles} bg-red-300`]: background === 'red',
+				},
+				className
+			)}
+			{...props}
+		>
 			{children}
 		</p>
 	);
