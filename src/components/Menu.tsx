003a6f70ff22c5a9e31e25e960bf583083d41c21
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-29 18:17:35 +0000

    Delete unnecessary component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,8 +10,8 @@ const menuItems = [
     path: '/',
   },
   {
-    name: 'Articles',
-    path: '/articles',
+    name: 'RSS',
+    path: '/rss',
   },
 ]
 
@@ -19,33 +19,26 @@ export default function Menu() {
   const pathname = usePathname()
 
   return (
-    <header className="-ml-[12px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {menuItems.map(item => (
-              <Link
-                key={item.path}
-                href={item.path}
-                className={clsx(
-                  'transition-all duration-200',
-                  'hover:text-neutral-800 dark:hover:text-neutral-200',
-                  'flex align-middle',
-                  'relative py-1 px-2 m-1',
-                  pathname === item.path
-                    ? 'font-semibold text-neutral-900 dark:text-neutral-100'
-                    : 'font-normal text-neutral-500 dark:text-neutral-400',
-                )}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </nav>
+    <nav className="-ml-3 flex flex-row items-start md:overflow-auto md:relative mb-12">
+      <div className="flex flex-row space-x-0 pr-10">
+        {menuItems.map(item => (
+          <Link
+            key={item.path}
+            href={item.path}
+            className={clsx(
+              'transition-all duration-200',
+              'hover:text-neutral-800 dark:hover:text-neutral-200',
+              'flex align-middle',
+              'relative py-1 px-2 m-1',
+              pathname === item.path
+                ? 'font-semibold text-neutral-900 dark:text-neutral-100'
+                : 'font-normal text-neutral-500 dark:text-neutral-400',
+            )}
+          >
+            {item.name}
+          </Link>
+        ))}
       </div>
-    </header>
+    </nav>
   )
 }
