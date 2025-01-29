ff5014b6a2931f80c1940b13183919b450e3fefa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-29 18:24:18 +0000

    Fix social images

--- src/app/articles/clean-code-robert-c-martin-summary-with-typescript-examples/data.ts
+++ src/app/articles/clean-code-robert-c-martin-summary-with-typescript-examples/data.ts
@@ -1,13 +1,16 @@
-import { type IArticle } from '@/library/articles'
+import { Article, tags, writers } from '@/types'
 
-import cleanCode from '../../../../public/images/social-png/clean-code-robert-c-martin-summary.png'
+import socialImage from '../../../../public/images/clean-code-robert-c-martin-summary.png'
+import featuredImage from './images/clean-code-robert-c-martin-summary.png'
 
-export const article: IArticle = {
-  writer: `Dan Edwards`,
+export const article: Article = {
+  writer: writers.danEdwards,
   date: '2024-09-10',
   title: `Clean Code by Robert C. Martin: summary with TypeScript examples`,
-  description: `Explore the key principles of writing maintainable and elegant code in our comprehensive review of Robert C. Martin's 'Clean Code'. This essential guide for software developers offers practical examples and timeless advice for crafting high-quality code.`,
-  tags: ['Book review'],
+  metaDescription: `Explore the key principles of writing maintainable and elegant code in our comprehensive review of Robert C. Martin's 'Clean Code'. This essential guide for software developers offers practical examples and timeless advice for crafting high-quality code.`,
+  tags: [tags.typescript, tags.bookReview],
   keywords: `clean code, robert c. martin, book review, software development, coding best practices, maintainable code, readable code, refactoring techniques, software craftsmanship, meaningful names, function design, error handling, unit testing, code patterns`,
-  featuredImage: cleanCode,
+  featuredImage,
+  socialImage,
+  slug: 'clean-code-robert-c-martin-summary-with-typescript-examples',
 }
