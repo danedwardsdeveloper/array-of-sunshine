8a6a492ceb75e68b1131fa43b9bf7ef88254fd3a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-11 22:04:31 +0000

    Update url handling

--- next.config.ts
+++ next.config.ts
@@ -1,65 +1,48 @@
-import { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
+// ToDo: add redirect from fly.io
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [320, 480, 576],
-    imageSizes: [16, 32, 64, 96, 128, 256],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'simpleanalyticsbadges.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'queue.simpleanalyticscdn.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.arrayofsunshine.co.uk',
-          },
-        ],
-        destination: 'https://arrayofsunshine.co.uk/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'array-of-sunshine.fly.dev',
-          },
-        ],
-        destination: 'https://arrayofsunshine.co.uk/:path*',
-        permanent: true,
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/robots.txt',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "connect-src 'self'; script-src 'none'; object-src 'none'; frame-src 'none'",
-          },
-        ],
-      },
-    ]
-  },
-}
+	output: "standalone",
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "www.arrayofsunshine.co.uk",
+					},
+				],
+				destination: "https://arrayofsunshine.co.uk/:path*",
+				permanent: true,
+			},
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "array-of-sunshine.fly.dev",
+					},
+				],
+				destination: "https://arrayofsunshine.co.uk/:path*",
+				permanent: true,
+			},
+		];
+	},
+	async headers() {
+		return [
+			{
+				source: "/robots.txt", // Can't remember what this is for now...
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value:
+							"connect-src 'self'; script-src 'none'; object-src 'none'; frame-src 'none'",
+					},
+				],
+			},
+		];
+	},
+};
 
-export default nextConfig
+export default nextConfig;
