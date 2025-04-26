d4a136af4e1e78b94d597a33f58bfd827fad3db3
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-26 21:36:49 +0100

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,27 +1,30 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
-import { defaultMetaDescription, defaultMetaTitle } from '@/library/articleMetadata'
+import {
+	defaultMetaDescription,
+	defaultMetaTitle,
+} from "@/library/constants/definitions/metadata";
 
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: defaultMetaTitle,
-    short_name: 'Array of Sunshine',
-    description: defaultMetaDescription,
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#000',
-    theme_color: '#000',
-    icons: [
-      {
-        src: '/favicon/icon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/favicon/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-  }
+	return {
+		name: defaultMetaTitle,
+		short_name: "Array of Sunshine",
+		description: defaultMetaDescription,
+		start_url: "/",
+		display: "standalone",
+		background_color: "#000",
+		theme_color: "#000",
+		icons: [
+			{
+				src: "/favicon/icon-192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/favicon/icon-512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	};
 }
