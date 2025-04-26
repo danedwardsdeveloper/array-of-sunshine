548979a169fd4e7674f3a774c723a1af5651bfa0
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-26 21:35:48 +0100

    Update configuration

--- next.config.ts
+++ next.config.ts
@@ -1,48 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-// ToDo: add redirect from fly.io
 const nextConfig: NextConfig = {
-	output: "standalone",
-	async redirects() {
-		return [
-			{
-				source: "/:path*",
-				has: [
-					{
-						type: "host",
-						value: "www.arrayofsunshine.co.uk",
-					},
-				],
-				destination: "https://arrayofsunshine.co.uk/:path*",
-				permanent: true,
-			},
-			{
-				source: "/:path*",
-				has: [
-					{
-						type: "host",
-						value: "array-of-sunshine.fly.dev",
-					},
-				],
-				destination: "https://arrayofsunshine.co.uk/:path*",
-				permanent: true,
-			},
-		];
+	output: 'standalone',
+	devIndicators: {
+		appIsrStatus: false,
 	},
-	async headers() {
-		return [
-			{
-				source: "/robots.txt", // Can't remember what this is for now...
-				headers: [
-					{
-						key: "Content-Security-Policy",
-						value:
-							"connect-src 'self'; script-src 'none'; object-src 'none'; frame-src 'none'",
-					},
-				],
-			},
-		];
-	},
-};
+}
 
-export default nextConfig;
+export default nextConfig
