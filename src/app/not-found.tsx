1858a24b3a0058213f388f13c7f6cf9220558f05
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-26 21:36:57 +0100

    Add ArticlesList

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,17 @@
+import ArticlesList from "@/components/ArticlesList";
+
 export default function NotFound() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">404 - Page Not Found</h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
-    </section>
-  )
+	return (
+		<section>
+			<h1 className="mb-8 text-2xl font-semibold">Page not found</h1>
+			<p className="mb-8 text-balance">
+				Sorry, I have probably updated the URL or deleted whatever you were
+				looking for.
+			</p>
+			<h2 className="mb-8 text-lg font-semibold">
+				Were you looking for one of these?
+			</h2>
+			<ArticlesList />
+		</section>
+	);
 }
